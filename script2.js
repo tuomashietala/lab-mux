aa7467124e8e2e25983323a32554023461ff899b
tuomashietala <tuomas.hietala@student.lab.fi>
2022-12-02 00:51:29 +0200

    s

---
+++ script2.js
@@ -0,0 +1,17 @@
+
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+
+document.addEventListener('click', ({ target }) => {
+  if (!target.closest('#menupop')) {
+    document.querySelector('#myLinks').style.display = 'none'
+  }
+})
